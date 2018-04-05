968eadd45481006edf0bc80aac9c0bf2839ebfbb
Zheng Jiang <none@none>
2018-04-05 16:08:28 +0200

    1.3

--- mod.lua
+++ mod.lua
@@ -1,7 +1,7 @@
 ﻿function data()
     return {
         info = {
-            minorVersion = 2,
+            minorVersion = 3,
             severityAdd = "NONE",
             severityRemove = "CRITICAL",
             name = _("name"),
