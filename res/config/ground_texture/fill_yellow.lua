bf77f3d786b380d03b70003a70f886b902197484
Zheng Jiang <none@none>
2018-10-06 22:40:41 +0200

    ground textures

---
+++ res/config/ground_texture/fill_yellow.lua
@@ -0,0 +1,11 @@
+local tu = require "texutil"
+
+function data()
+return {
+	detailTex = tu.makeTextureMipmapClamp("ground_texture/fill_yellow.tga", true, true),
+	detailNrmlTex = tu.makeTextureMipmapClamp("ground_texture/tree_ground_nrml.dds", true, true, true),
+	detailSize = { 4.0, 4.0 },
+
+	priority = 20
+}
+end
